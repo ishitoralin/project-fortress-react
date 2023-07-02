c4a4dfcbdee93cd45e763d23c07f119b41c3daa4
shihu <miraculous0218@hotmail.com>
2023-07-02 14:04:26 +0800

    0702

--- pages/shoppingcart/index.js
+++ pages/shoppingcart/index.js
@@ -1,3 +1,19 @@
+import { Title } from '@mui/icons-material';
+import styles from './shoppingcart.module.css';
+
 export default function ShoppingCart() {
-  return <h1>ShoppingCart</h1>;
+  const shoppingState = {
+    border: '2px solid red',
+    width: '100%',
+    height: '150px',
+    display: 'flex',
+  };
+
+  return (
+    <div style={shoppingState}>
+      <div className={styles.words}>訂單確認</div>
+      <div>買家資訊</div>
+      <div>訂單明細</div>
+    </div>
+  );
 }
