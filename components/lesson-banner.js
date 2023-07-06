fad7dd97e52fe4c38e99c3ebb41da8991791bc58
HaoHan <haohan11pfer@protonmail.com>
2023-07-07 00:41:26 +0800

    styled banner

--- components/lesson-banner.js
+++ components/lesson-banner.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react';
 
 import Image from 'next/image';
-import { Box, Typography } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
 import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import WhiteTheme from '@/context/Theme/white-theme';
 
-import absImg from '@/assets/abs.jpg';
-import abs1Img from '@/assets/abs1.jpg';
-import core from '@/assets/core.jpg';
-import functional from '@/assets/functional.jpg';
-import hiit from '@/assets/HIIT.jpg';
-import oxgen1 from '@/assets/oxgen1.jpg';
+import absImg from '@/public/lesson-img/abs.jpg';
+import abs1Img from '@/public/lesson-img/abs1.jpg';
+import core from '@/public/lesson-img/core.jpg';
+import functional from '@/public/lesson-img/functional.jpg';
+import hiit from '@/public/lesson-img/HIIT.jpg';
+import oxgen1 from '@/public/lesson-img/oxgen1.jpg';
 
 const bannerData = [
   {
@@ -29,7 +32,7 @@ const bannerData = [
     img: core,
     title: 'Lesson Title here',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam cum reprehenderit consectetur debitis culpa rem ipsum optio voluptatibus nostrum asperiores?',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam cum reprehenderit consectetur debitis culpa rem ipsum optio voluptatibus nostrum asperiores?Lorem ipsum adipisicing elit. Magnam cum  ipsum optio voluptatibus nostrum asperiores?',
   },
   {
     img: functional,
@@ -147,98 +150,157 @@ const Banner = () => {
   };
 
   return (
-    <Box sx={bannerStyle}>
-      <ChevronLeftRoundedIcon
-        sx={{ ...arrowStyle, '--od': '1', left: 'var(--gap)' }}
-        onClick={goLeft}
-      />
-      <ChevronRightRoundedIcon
-        sx={{ ...arrowStyle, '--od': '-1', right: 'var(--gap)' }}
-        onClick={goRight}
-      />
-      <Box
-        sx={{
-          ...carouselStyle,
-          left: `${-100 * leftPosi}%`,
-          transition: `${transi ? slideTime : '0'}ms ease-in-out`,
-        }}
-      >
-        {order.map((item, index) => (
-          <Box
-            key={item}
-            sx={{ position: 'relative', width: '100%', height: '100%' }}
-          >
+    <WhiteTheme>
+      <Box sx={bannerStyle}>
+        <ChevronLeftRoundedIcon
+          sx={{ ...arrowStyle, '--od': '1', left: 'var(--gap)' }}
+          onClick={goLeft}
+        />
+        <ChevronRightRoundedIcon
+          sx={{ ...arrowStyle, '--od': '-1', right: 'var(--gap)' }}
+          onClick={goRight}
+        />
+        <Box
+          sx={{
+            ...carouselStyle,
+            left: `${-100 * leftPosi}%`,
+            transition: `${transi ? slideTime : '0'}ms ease-in-out`,
+          }}
+        >
+          {order.map((item, index) => (
             <Box
-              sx={{
-                position: 'absolute',
-                inset: '25% 20%',
-                // border: '2px solid gold',
-                zIndex: 1,
-                ':before': {
-                  content: '""',
-                  position: 'absolute',
-                  top: '-11%',
-                  left: '-7%',
-                  // display: 'block',
-                  width: '100%',
-                  height: '100%',
-                  border: '2px solid white',
-                },
-                ':after': {
-                  content: '""',
-                  position: 'absolute',
-                  top: '-7%',
-                  left: '-5%',
-                  // display: 'block',
-                  width: '100%',
-                  height: '100%',
-                  border: '2px solid white',
-                },
-              }}
+              key={item}
+              sx={{ position: 'relative', width: '100%', height: '100%' }}
             >
-              <Typography
-                variant={'h3'}
+              <Box
                 sx={{
-                  // position: 'absolute',
-                  // top: '50%',
-                  color: 'white',
-                  // border: '2px solid gold',
+                  position: 'absolute',
+                  inset: '20% 15%',
+                  padding: '3% 5%',
+                  border: '2px solid white',
                   zIndex: 1,
+                  ':before': {
+                    content: '""',
+                    position: 'absolute',
+                    top: '-20%',
+                    left: '-7.5%',
+                    backdropFilter: 'blur(10px)',
+                    width: '40%',
+                    height: '24%',
+                    border: '3px solid white',
+                    transition: '1s',
+                    pointerEvents: 'none',
+                    backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='30' height='30' patternTransform='scale(1.5) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(25, 0%, 100%, 0)'/><path d='M0 22.5h30v15H0zm15-15h30v15H15m-30-15h30v15h-30zm15-15h30v15H0z'  stroke-width='1.5' stroke='hsla(38, 0%, 100%, 1)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,-14)' fill='url(%23a)'/></svg>")`,
+                  },
+                  ':after': {
+                    content: '""',
+                    position: 'absolute',
+                    top: '90%',
+                    left: '67%',
+                    width: '40%',
+                    height: '24%',
+                    border: '3px solid white',
+                    backdropFilter: 'blur(10px)',
+                    transition: '1s',
+                    pointerEvents: 'none',
+                    backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='30' height='30' patternTransform='scale(1.5) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(25, 0%, 100%, 0)'/><path d='M0 22.5h30v15H0zm15-15h30v15H15m-30-15h30v15h-30zm15-15h30v15H0z'  stroke-width='1.5' stroke='hsla(38, 0%, 100%, 1)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,-14)' fill='url(%23a)'/></svg>")`,
+                  },
+                  ':hover:after, :hover:before': {
+                    transitionDelay: '.5s',
+                    width: '100%',
+                    height: '100%',
+                    top: '0',
+                    left: '0',
+                    backdropFilter: 'blur(0px)',
+                    opacity: '0.05',
+                  },
                 }}
               >
-                {bannerData[item]['title']}
-              </Typography>
-              <Typography
-                variant={'h5'}
-                sx={{
-                  // position: 'absolute',
-                  // top: '50%',
-                  color: 'white',
-                  // border: '2px solid gold',
-                  padding: '2rem 5rem',
-                  zIndex: 1,
+                <Typography
+                  variant={'h4'}
+                  sx={{
+                    // position: 'absolute',
+                    // top: '50%',
+                    color: 'white',
+                    zIndex: 1,
+                  }}
+                >
+                  {bannerData[item]['title']}
+                </Typography>
+                <Typography
+                  variant={'h5'}
+                  sx={{
+                    color: 'white',
+                    marginTop: '1rem',
+                    paddingInline: '6rem',
+                    zIndex: 1,
+                  }}
+                >
+                  {bannerData[item]['description']}
+                </Typography>
+                <Button
+                  endIcon={
+                    <TrendingFlatIcon
+                      sx={{
+                        marginLeft: '.5rem',
+                        transform: 'translateY(-1px) scale(1.2)',
+                      }}
+                    />
+                  }
+                  variant="outlined"
+                  sx={{
+                    position: 'absolute',
+                    left: '10%',
+                    bottom: '10%',
+                    boxShadow: 'none',
+                    borderWidth: '3px',
+                    borderRadius: '30px',
+                    paddingInline: '1rem',
+                    fontWeight: 'bold',
+                    fontSize: '1.1rem',
+                    paddingRight: '3rem',
+                    paddingLeft: '3rem',
+                    overflow: 'hidden',
+                    ':hover': {
+                      borderWidth: '3px',
+                      color: 'black',
+                    },
+                    ':before': {
+                      content: '""',
+                      position: 'absolute',
+                      left: '-100%',
+                      width: '100%',
+                      height: '100%',
+                      bgcolor: 'white',
+                      transition: '.5s',
+                      zIndex: '-1',
+                    },
+                    ':hover:before': {
+                      left: 0,
+                    },
+                  }}
+                >
+                  了解更多
+                </Button>
+              </Box>
+              <Image
+                src={bannerData[item]['img']}
+                priority
+                alt="bannerimg"
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  left: `${index * 100}%`,
+                  objectFit: 'cover',
+                  objectPosition: '50% 50%',
+                  filter: 'brightness(40%)',
                 }}
-              >
-                {bannerData[item]['description']}
-              </Typography>
+              />
             </Box>
-            <Image
-              src={bannerData[item]['img']}
-              priority
-              alt="bannerimg"
-              style={{
-                width: '100%',
-                height: '100%',
-                left: `${index * 100}%`,
-                objectFit: 'cover',
-                objectPosition: '50% 50%',
-                filter: 'brightness(40%)',
-              }}
-            />
-          </Box>
-        ))}
+          ))}
+        </Box>
       </Box>
-    </Box>
+    </WhiteTheme>
   );
 };
 
