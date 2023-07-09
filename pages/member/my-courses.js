b21d984e3b5128d857c6d4d9dab8d9b735170978
LQTjim <jim6050482@hotmail.com>
2023-07-09 08:46:47 +0800

    add:member-sub-nav layout

--- pages/member/my-courses.js
+++ pages/member/my-courses.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import MemberLayout from '@/components/layout/memberLayout';
 
 export default function MyCourses() {
   return <div>my-course</div>;
 }
+MyCourses.getLayout = (page) => <MemberLayout>(page)</MemberLayout>;
--- pages/member/my-orders.js
+++ pages/member/my-orders.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import MemberLayout from '@/components/layout/memberLayout';
 
 export default function MyOrders() {
   return <div>my-order</div>;
 }
+MyOrders.getLayout = (page) => <MemberLayout>(page)</MemberLayout>;
