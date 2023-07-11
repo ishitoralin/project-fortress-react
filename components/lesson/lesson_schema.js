70ff5a33463f9ae5ec157a63fa56d06c22a3c8a4
HaoHan <haohan11pfer@protonmail.com>
2023-07-12 00:21:38 +0800

    add lesson schema

---
+++ components/lesson/lesson_schema.js
@@ -0,0 +1,15 @@
+// total: 12
+export const lessonSchema = [
+  'name',
+  'img',
+  'coachname',
+  'description',
+  'price',
+  'enrolled',
+  'limit',
+  'location',
+  'time',
+  'period',
+  'category',
+  'tags',
+];
