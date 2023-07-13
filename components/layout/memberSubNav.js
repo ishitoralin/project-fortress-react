6598b1a3bcbd92df3da22bd53f790ddf9a90f4c1
LQTjim <jim6050482@hotmail.com>
2023-07-13 17:41:39 +0800

    update:member sub-nav RWD

--- components/layout/memberSubNav.js
+++ components/layout/memberSubNav.js
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
 import styles from '@/styles/member.module.css';
+import CUIButton from '../customUI/cui-button';
+import { Box } from '@mui/material';
 const linkDetail = [
   { href: '/member', title: '會員資料' },
   { href: '/member/my-products', title: '收藏商品' },
@@ -11,32 +13,57 @@ const linkDetail = [
 ];
 export default function MemberSubNav() {
   const [route, setRoute] = useState();
+  const [expand, setExpand] = useState(false);
   const router = useRouter();
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setRoute(router?.route?.split('/member/')[1]);
     }
   }, [router]);
-
+  const handleExpand = () => {
+    setExpand((prev) => !prev);
+  };
   return (
-    <section className={`${styles['sub-nav-section']}`}>
-      <div className={`${styles['sub-nav-container']}`}>
-        {linkDetail.map((el, i) => (
-          <Link
-            href={`${el.href}`}
-            key={i}
-            className={`${styles['sub-nav-link']}`}
-          >
-            {route === el?.href?.split('/member/')[1] && (
-              <motion.div
-                layoutId="sub-nav-active"
-                className={`${styles['sub-nav-active']}`}
-              />
-            )}
-            <h6 className={`${styles['sub-nav-title']}`}>{el.title}</h6>
-          </Link>
-        ))}
-      </div>
-    </section>
+    <>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}
+      >
+        <CUIButton
+          className={`${styles['expand-button']}`}
+          onClick={handleExpand}
+        >
+          {
+            linkDetail.find((el) => route === el?.href?.split('/member/')[1])
+              ?.title
+          }
+        </CUIButton>
+      </Box>
+      <section
+        className={`${styles['sub-nav-section']} ${
+          expand ? styles['expand'] : ''
+        }`}
+      >
+        <div className={`${styles['sub-nav-container']}`}>
+          {linkDetail.map((el, i) => (
+            <Link
+              href={`${el.href}`}
+              key={i}
+              className={`${styles['sub-nav-link']}`}
+              onClick={() => {
+                setExpand(false);
+              }}
+            >
+              {route === el?.href?.split('/member/')[1] && (
+                <motion.div
+                  layoutId="sub-nav-active"
+                  className={`${styles['sub-nav-active']}`}
+                />
+              )}
+              <h6 className={`${styles['sub-nav-title']}`}>{el.title}</h6>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
--- pages/member/index.js
+++ pages/member/index.js
@@ -1,52 +1,155 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MemberCenterLayout from '@/components/layout/memberCenterLayout';
 import styles from '@/styles/member.module.css';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
+import CUITextField from '@/components/customUI/cui-textfield';
+import CUIDatePicker from '@/components/customUI/cui-date-picker';
+import CUISelect from '@/components/customUI/cui-select';
+import CUIButton from '@/components/customUI/cui-button';
+
 const validationSchema = yup.object({});
 export default function Index() {
+  const initialData = {
+    name: '',
+    email: '',
+    sex: '',
+    address: '',
+    birth: '',
+    mobile: '',
+  };
+  const formikInitialData = {
+    sex: '',
+    address: '',
+    birth: '',
+    mobile: '',
+  };
+  const [data, setData] = useState(initialData);
+  const [displayData, setDisplayData] = useState(initialData);
+  const formik = useFormik({
+    initialValues: formikInitialData,
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      const valuesCopied = { ...values };
+      delete valuesCopied['name'];
+      delete valuesCopied['email'];
+      //抹掉不必要欄位 valuesCopied為要 UPDATE的資料
+      console.log(JSON.stringify(valuesCopied, null, 2));
+    },
+  });
+  useEffect(() => {
+    setData({
+      sex: '不透露',
+      address: '台北市北投區復興二路29號',
+      birth: '2023-05-10',
+      mobile: '0918183537',
+    });
+    formik.setValues({
+      sex: '不透露',
+      address: '台北市北投區復興二路29號',
+      birth: '2023-05-10',
+      mobile: '0918183537',
+    });
+    setData({
+      name: '王曉明',
+      email: 'asdf@gmail.com',
+      sex: '不透露',
+      address: '台北市北投區復興二路29號',
+      birth: '2023-05-10',
+      mobile: '0918183537',
+    });
+    setDisplayData({
+      name: '王曉明',
+      email: 'asdf@gmail.com',
+      sex: '不透露',
+      address: '台北市北投區復興二路29號',
+      birth: '2023-05-10',
+      mobile: '0918183537',
+    });
+  }, []);
+  useEffect(() => {
+    setDisplayData({ ...data, ...formik.values });
+  }, [formik.values]);
   const filed = [
     {
       label: '姓名',
       placeholder: '',
       name: 'name',
       type: 'text',
-      value: '',
-      disable: true,
+      disabled: true,
     },
     {
       label: '電子信箱',
       placeholder: '',
       name: 'email',
       type: 'text',
-      value: '',
-      disable: true,
+      disabled: true,
     },
     {
       label: '性別',
       placeholder: '',
-      name: 'gender',
-      type: '',
-      value: '',
-      disable: true,
+      name: 'sex',
+      type: 'select',
+      disabled: false,
+    },
+    { label: '地址', placeholder: '請輸入地址', name: 'address', type: 'text' },
+    { label: '生日', placeholder: '請輸入生日', name: 'birth', type: 'date' },
+    {
+      label: '手機',
+      placeholder: 'ex:0912345678',
+      name: 'mobile',
+      type: 'text',
     },
   ];
-  const formik = useFormik({
-    initialValues: { email: '', password: '' },
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2));
-    },
-  });
   return (
-    <div className="center-container">
+    <div className={`${styles['center-container']}`}>
       <div className={`${styles['photo']}`}>
         <CameraAltIcon className={`${styles['camera']}`} fontSize="large" />
       </div>
-      <div className={`${styles['info']}`}>
+      <form onSubmit={formik.handleSubmit} className={`${styles['info']}`}>
         {filed.map((el) => {
-          const { label, name, placeholder, type } = el;
+          const { label, name, placeholder, type, disabled = false } = el;
+          if (disabled) {
+            return (
+              <CUITextField
+                key={name}
+                value={displayData[name]}
+                label={label}
+                disabled
+              />
+            );
+          }
+          if (type === 'date') {
+            return (
+              <CUIDatePicker
+                key={name}
+                value={displayData[name]}
+                label={label}
+                format="YYYY-MM-D"
+                onChange={(e) => {
+                  formik.setValues((v) => {
+                    return { ...v, [name]: e };
+                  });
+                }}
+              />
+            );
+          }
+          if (type === 'select') {
+            return (
+              <CUISelect
+                label={label}
+                options={['男', '女', '不透露']}
+                key={name}
+                value={displayData[name]}
+                onChange={(e) => {
+                  formik.setValues((v) => {
+                    return { ...v, [name]: e.target.value };
+                  });
+                }}
+              />
+            );
+          }
           return (
             <CUITextField
               key={name}
@@ -54,17 +157,26 @@ export default function Index() {
               placeholder={placeholder}
               name={name}
               type={type}
-              value={formik.values[name]}
+              value={displayData[name]}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
               error={formik.touched[name] && Boolean(formik.errors[name])}
               helperText={formik.touched[name] && formik.errors[name]}
               autoComplete="off"
+              disabled={disabled}
               sx={{ marginBottom: '-15px' }}
             />
           );
         })}
-      </div>
+        <CUIButton
+          onClick={() => {
+            formik.setValues(data);
+          }}
+        >
+          重置
+        </CUIButton>
+        <CUIButton type="submit">儲存並變更</CUIButton>
+      </form>
     </div>
   );
 }
