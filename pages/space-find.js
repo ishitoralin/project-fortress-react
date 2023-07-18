fbec298914f75f250220121f8d6b5d5f000ffbe0
LQTjim <jim6050482@hotmail.com>
2023-07-18 15:11:20 +0800

    fix:space-find

--- pages/space-find.js
+++ pages/space-find.js
@@ -1,1 +1,12 @@
-export default () => <h1>Space find</h1>;
+import dynamic from 'next/dynamic';
+import React from 'react';
+const SpaceFindComponent = dynamic(
+  () => import('@/components/space-find-component'),
+  {
+    ssr: false,
+  }
+);
+
+export default function SpaceFind() {
+  return <SpaceFindComponent />;
+}
