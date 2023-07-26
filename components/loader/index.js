2ad3b75a2e04f756ba6800d2546fc08520e9eb75
LQTjim <jim6050482@hotmail.com>
2023-07-27 06:08:22 +0800

    update:page/member/index connect to backend,read the data from backend

--- components/loader/index.js
+++ components/loader/index.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import styles from './loader.module.css';
+import { useRouter } from 'next/router';
 
 function Loader() {
+  const router = useRouter();
+  useEffect(() => {
+    //TODO 邏邏輯可能要再修正 這邊強制兩秒就跳轉首頁
+    const timeID = setTimeout(() => {
+      router.push('/');
+    }, 3000);
+    return () => {
+      clearTimeout(timeID);
+    };
+  }, [router]);
   return (
     <div className={`${styles['loader-p']}`}>
       {/* <div className="loader loader--spinningDisc"></div> */}
