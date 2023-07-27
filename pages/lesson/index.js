db90762514ff9984dbe2329196eaa5bb35c413e1
haohan <haohan11per@protonmail.com>
2023-07-27 17:43:11 +0800

    handle filter input

--- pages/lesson/index.js
+++ pages/lesson/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import {
@@ -20,6 +20,7 @@ import CUISelect from '@/components/customUI/cui-select';
 import CUISlider from '@/components/customUI/cui-slider';
 import CUIDatePicker from '@/components/customUI/cui-date-picker';
 import CUIFilter from '@/components/customUI/cui-filter';
+import { indexOf } from 'lodash';
 
 const filterIconStyle = {
   visibility: 'hidden',
@@ -45,8 +46,10 @@ export const getStaticProps = async () => {
 };
 
 const DEFAULTDISPLAYMODE = 'list';
-
 const LESSON_BASEURL = 'http://localhost:3001/lesson';
+
+const initPrice = [200, 1500];
+const initStep = 50;
 
 const queryDatasCache = new Map();
 
@@ -54,16 +57,32 @@ const LessionPage = (props) => {
   const router = useRouter();
   const [lessons, setLessons] = useState([]);
   const [queryObject, setQueryObject] = useState({});
+  const keywordRef = useRef();
+  const dateAfterRef = useRef();
+  const dateBeforeRef = useRef();
+  const priceRef = useRef();
   const location = queryObject.location;
   const [displayMode, setDisplayMode] = useState(DEFAULTDISPLAYMODE);
 
   const [tags, setTags] = useState(props.tags);
-  const [selectTags, setSelecTags] = useState([]);
+  const [selectTags, setSelectTags] = useState([]);
 
   const [filterShow, setFilterShow] = useState(false);
 
+  const getFilterValues = () => ({
+    keyword: keywordRef.current.value,
+    dateAfter: dateAfterRef.current.value,
+    dateBefore: dateBeforeRef.current.value,
+    price: priceRef.current,
+    tags: selectTags,
+  });
+
   const setLocation = (newLocation) => {
-    setQueryObject((prev) => ({ ...prev, location: newLocation }));
+    setQueryObject((prev) => ({
+      ...prev,
+      ...getFilterValues(),
+      location: newLocation,
+    }));
   };
 
   const showFilter = () => setFilterShow(true);
@@ -72,10 +91,22 @@ const LessionPage = (props) => {
 
   const queryLessons = async (baseUrl, queryObj) => {
     const response = {};
+    Object.keys(queryObj).forEach(
+      (key) =>
+        (queryObj[key] === undefined || queryObj[key] === '') &&
+        delete queryObj[key]
+    );
     const fetchUrl = Object.entries(queryObj).reduce(
-      (url, [key, value]) => `${url}${key}=${value}&`,
+      (url, [key, value]) =>
+        Array.isArray(value)
+          ? value.reduce(
+              (item, nextItem) => `${item}${key}[]=${nextItem}&`,
+              url
+            )
+          : `${url}${key}=${value}&`,
       `${baseUrl}?`
     );
+
     try {
       const res = await fetch(fetchUrl);
       const datas = await res.json();
@@ -91,6 +122,11 @@ const LessionPage = (props) => {
   };
 
   const pushRouter = (queryObj) => {
+    Object.keys(queryObj).forEach(
+      (key) =>
+        (queryObj[key] === undefined || queryObj[key] === '') &&
+        delete queryObj[key]
+    );
     router.push(
       {
         pathname: '',
@@ -123,8 +159,45 @@ const LessionPage = (props) => {
     if (!router.isReady) return;
     setQueryObject({
       ...router.query,
-      location: router.query.location || 'taipei',
+      location:
+        ['taipei', 'taichung', 'kaohsiung'].indexOf(router.query.location) ===
+        -1
+          ? 'taipei'
+          : router.query.location,
     });
+    const queryTags = router.query.tags;
+    if (queryTags) {
+      const tagsArray = Array.isArray(queryTags)
+        ? Array.from(new Set(queryTags))
+        : [queryTags];
+      const initSelectTags = tagsArray.filter(
+        (tag) => props.tags.indexOf(tag) !== -1
+      );
+      setSelectTags(initSelectTags);
+      setTags(props.tags.filter((tag) => initSelectTags.indexOf(tag) === -1));
+    }
+    keywordRef.current.value = router.query.keyword || '';
+    dateAfterRef.current.value = router.query.dateAfter;
+    dateBeforeRef.current.value = router.query.dateBefore;
+    priceRef.current = (() => {
+      if (!router.query.price) return initPrice;
+      const thumbs = [
+        parseInt(router.query.price[0]),
+        parseInt(router.query.price[1]),
+      ];
+      if (
+        isNaN(thumbs[0]) ||
+        isNaN(thumbs[1]) ||
+        thumbs[0] % initStep !== 0 ||
+        thumbs[1] % initStep !== 0 ||
+        thumbs[0] >= thumbs[1] ||
+        thumbs[0] < initPrice[0] ||
+        thumbs[1] > initPrice[1] ||
+        thumbs[1] - thumbs[0] < initStep
+      )
+        return initPrice;
+      return thumbs;
+    })();
   }, [router.isReady]);
 
   return (
@@ -153,11 +226,15 @@ const LessionPage = (props) => {
               }
               color={'steel_grey'}
               label="篩選器"
+              onClick={() => {
+                setQueryObject((prev) => ({ ...prev, ...getFilterValues() }));
+              }}
               items={[
                 <CUISearch
-                  key={'search'}
+                  key={keywordRef.current ? 'search' : 'search-pending'}
                   color={'steel_grey'}
                   label="課程關鍵字"
+                  inputRef={keywordRef}
                   placeholder="教練名稱、課程名稱..."
                 />,
                 <>
@@ -176,7 +253,7 @@ const LessionPage = (props) => {
                         (tag) => tag !== event.target.value
                       );
                       setTags(newTags);
-                      setSelecTags(newSelect);
+                      setSelectTags(newSelect);
                     }}
                   />
                   <Box key={'tag-box'}>
@@ -195,18 +272,49 @@ const LessionPage = (props) => {
                               props.tags.indexOf(t1) - props.tags.indexOf(t2)
                           );
                           setTags(newTags);
-                          setSelecTags(newState);
+                          setSelectTags(newState);
                         }}
                       />
                     ))}
                   </Box>
                 </>,
                 <CUIDatePicker
-                  key={'date-picker'}
-                  label="課程日期"
+                  key={'date-after'}
+                  label="此日期後的課程"
                   color={'steel_grey'}
+                  value={(() => {
+                    if (!dateAfterRef.current) return undefined;
+                    return dateAfterRef.current.value === '' ||
+                      dateAfterRef.current.value === 'YYYY/MM/DD'
+                      ? undefined
+                      : dateAfterRef.current.value;
+                  })()}
+                  inputRef={dateAfterRef}
                 />,
-                <CUISlider key={'slider'} label="價格範圍" />,
+                <CUIDatePicker
+                  key={'date-before'}
+                  label="此日期前的課程"
+                  color={'steel_grey'}
+                  value={(() => {
+                    if (!dateBeforeRef.current) return undefined;
+                    return dateBeforeRef.current.value === '' ||
+                      dateBeforeRef.current.value === 'YYYY/MM/DD'
+                      ? undefined
+                      : dateBeforeRef.current.value;
+                  })()}
+                  inputRef={dateBeforeRef}
+                />,
+                <CUISlider
+                  key={priceRef.current ? 'price-slider' : 'p-s-pending'}
+                  label="價格範圍"
+                  value={(() => {
+                    if (!priceRef.current) return undefined;
+                    return [priceRef.current[0], priceRef.current[1]];
+                  })()}
+                  min={initPrice[0]}
+                  max={initPrice[1]}
+                  onChange={(price) => (priceRef.current = price)}
+                />,
               ]}
             />
             <RightSide
