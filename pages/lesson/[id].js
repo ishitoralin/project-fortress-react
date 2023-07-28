47accfe98535e3174e15220a5b536e8ba3695971
haohan <haohan11per@protonmail.com>
2023-07-28 15:42:31 +0800

    show real data from backend

--- pages/lesson/[id].js
+++ pages/lesson/[id].js
@@ -32,50 +32,58 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  const res = await fetch(`http://localhost:3001/lesson?category=${id}`);
-  const data = await res.json();
+  const getCategoryUrl = `http://localhost:3001/lesson/categories?id=${id}`;
+  const getLessonsUrl = `http://localhost:3001/lesson?category=${id}`;
+
+  const [category, lessons] = await Promise.all(
+    [getCategoryUrl, getLessonsUrl].map(async (url) => {
+      const res = await fetch(url);
+      return await res.json();
+    })
+  );
+
+  const lessonsAddCoachImg = lessons.map((lesson) => ({
+    ...lesson,
+    coach_img: (() => {
+      const [coach] = category.coachs.filter(
+        (coach) => coach.sid === lesson.coach_sid
+      );
+      return coach.img;
+    })(),
+  }));
 
   return {
     props: {
-      lessons: data,
+      category,
+      lessons: lessonsAddCoachImg,
     },
   };
 };
 
-const CertainLessonPage = ({ lessons }) => {
+const CertainLessonPage = ({ category, lessons }) => {
   return (
     <Box>
-      {/* {console.log(lessons)} */}
+      {console.log(category, lessons)}
       <BrickWallPaper />
       <Container sx={containerStyle}>
         <Box sx={cardBoxStyle}>
           <CUICard sx={imgBoxStyle}>
             <Image
               alt="lesson-img"
-              src="/lesson-img/functional.jpg"
+              src={`/lesson-img/${category.img}`}
+              blurDataURL={`/lesson-img/${category.img_base64}`}
               fill
               style={{ objectFit: 'cover' }}
             />
           </CUICard>
           <CUICard sx={cardBodyStyle}>
             <Typography variant="h4" sx={cardBodyTitle}>
-              瑜珈流動與冥想課程
+              {category.name}
             </Typography>
             <Typography variant="subtitle1" sx={cardBodyInfo}>
-              這個課程以流動的瑜伽動作和呼吸練習為基礎，融合了力量、靈活性和平衡。透過連貫的動作流，你將提升身體柔軟度、強化肌肉，同時培養身心的平靜和集中力。
+              {category.description}
             </Typography>
-            {[
-              '有氧',
-              '心肺',
-              '拳擊',
-              '格鬥',
-              '瑜珈',
-              '壺鈴',
-              '健力',
-              '健美',
-              '健體',
-              '核心',
-            ].map((tag, index) => (
+            {lessons[0].tags.map((tag, index) => (
               <Chip
                 key={index}
                 label={tag}
@@ -85,9 +93,9 @@ const CertainLessonPage = ({ lessons }) => {
             ))}
             <Box sx={coachNameBoxStyle}>
               <Typography variant="h6">指導教練</Typography>
-              {['可達鴨', '大蔥鴨'].map((name) => (
-                <Typography key={name} variant="h5">
-                  {name}
+              {category.coachs.map((coach) => (
+                <Typography key={coach.sid} variant="h5">
+                  {coach.nickname}
                 </Typography>
               ))}
             </Box>
@@ -95,44 +103,11 @@ const CertainLessonPage = ({ lessons }) => {
         </Box>
         <Box sx={lessonsBoxStyle}>
           <Grid container sx={lessonsCardGridStyle}>
-            {Array(10)
-              .fill({
-                lessonName: '水阻划船入門',
-                img: '/coach-img/emily.jpg',
-                time: '2024/05/06 14:00',
-                description:
-                  '這個課程將提供學員們一個初步的認識和實踐水阻划船的機會。學員們將學習基本的划船動作和安全知識。',
-                coachName: '蔡岱峯',
-                price: '600',
-                enrolled: 10,
-                limit: 20,
-                tags: [
-                  '有氧',
-                  '心肺',
-                  '拳擊',
-                  '格鬥',
-                  '瑜珈',
-                  '壺鈴',
-                  '健力',
-                  '健美',
-                  '健體',
-                  '核心',
-                ],
-                location: '台北館',
-              })
-              .map((lesson, index) => (
-                <Grid
-                  key={index}
-                  item
-                  xs={12}
-                  sm={11}
-                  md={8}
-                  lg={5.75}
-                  xl={5.75}
-                >
-                  {/* <LessonCard lesson={lesson} coachcard /> */}
-                </Grid>
-              ))}
+            {lessons.map((lesson, index) => (
+              <Grid key={index} item xs={12} sm={11} md={8} lg={5.75} xl={5.75}>
+                <LessonCard lesson={lesson} coachcard />
+              </Grid>
+            ))}
           </Grid>
         </Box>
       </Container>
