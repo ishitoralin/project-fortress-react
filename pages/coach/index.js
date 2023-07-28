e57d8e44f8f7f00095a7111b47a5de396b18509d
haohan <haohan11per@protonmail.com>
2023-07-28 18:00:00 +0800

    add skeleton to banner

--- pages/coach/index.js
+++ pages/coach/index.js
@@ -8,61 +8,46 @@ import BrickWallPaper from '@/components/brick-wallpaper';
 import UiButton from '@/components/hh/UiButton';
 
 const CoachListPage = () => {
-  const [location, setLocation] = useState(['Taipei', 'Taichung']);
+  const [location, setLocation] = useState(['taipei']);
 
   return (
     <Box>
       <BrickWallPaper scale={1.6} rotate={7.5} />
       <Container sx={{ paddingBlock: { xs: '1rem', sm: '3rem' } }}>
-        <Box>
-          <Box
+        <Box sx={{ textAlign: 'center', marginBlock: 5 }}>
+          <ToggleButtonGroup
+            value={location}
+            exclusive
+            aria-label="coach-location"
             sx={{
-              bgcolor: '#eee',
-              padding: 2,
-              borderRadius: '5px',
+              button: {
+                transition: '.3s',
+              },
+              'button:not(:last-child)': {
+                marginRight: { xs: '3rem', sm: '5rem' },
+              },
             }}
-          >
-            <CUISearch
-              placeholder="輸入教練名稱"
-              color={'steel_grey'}
-              label={'搜尋教練'}
-            />
-          </Box>
-          <Box sx={{ textAlign: 'center', marginBlock: 5 }}>
-            <ToggleButtonGroup
-              value={location}
-              exclusive
-              aria-label="coach-location"
-              sx={{
-                button: {
-                  transition: '.3s',
-                },
-                'button:not(:last-child)': {
-                  marginRight: { xs: '3rem', sm: '5rem' },
-                },
-              }}
-              onChange={(event, value) =>
-                setLocation(([...preValue]) => {
-                  const indexOfValue = preValue.indexOf(value);
-                  if (indexOfValue === -1) return [...preValue, value];
-                  if (preValue.length === 1) return preValue;
+            onChange={(event, value) =>
+              setLocation(([...preValue]) => {
+                const indexOfValue = preValue.indexOf(value);
+                if (indexOfValue === -1) return [...preValue, value];
+                if (preValue.length === 1) return preValue;
 
-                  preValue.splice(indexOfValue, 1);
-                  return preValue;
-                })
-              }
-            >
-              <UiButton disableRipple value="Taipei" aria-label="Taipei">
-                台北館
-              </UiButton>
-              <UiButton disableRipple value="Taichung" aria-label="Taichung">
-                台中館
-              </UiButton>
-              <UiButton disableRipple value="Kaohsiung" aria-label="Kaohsiung">
-                高雄館
-              </UiButton>
-            </ToggleButtonGroup>
-          </Box>
+                preValue.splice(indexOfValue, 1);
+                return preValue;
+              })
+            }
+          >
+            <UiButton disableRipple value="taipei" aria-label="taipei">
+              台北館
+            </UiButton>
+            <UiButton disableRipple value="taichung" aria-label="taichung">
+              台中館
+            </UiButton>
+            <UiButton disableRipple value="kaohsiung" aria-label="kaohsiung">
+              高雄館
+            </UiButton>
+          </ToggleButtonGroup>
         </Box>
         <Box
           sx={{
