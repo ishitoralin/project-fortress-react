8bde9c57a1dbd8fbe1f9496cfe405cc35055758c
max <max003315@gmail.com>
2023-08-08 15:50:39 +0800

    8/8 03

---
+++ components/product/page.js
@@ -0,0 +1,32 @@
+import Pagination from '@mui/material/Pagination';
+import { Stack } from '@mui/material';
+import styles from '@/components/member/member.module.css';
+
+import { useRouter } from 'next/router';
+export default function ProductPage({ data, cid }) {
+  const router = useRouter();
+
+  return (
+    <>
+      {parseInt(data?.totalPages) === 0 ? undefined : (
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          className={`${styles['pagination-container']}`}
+        >
+          <Pagination
+            variant="outlined"
+            shape="rounded"
+            count={data?.totalPages || 0}
+            siblingCount={0}
+            page={data.page}
+            onChange={(e, page) => {
+            //   console.log(router.pathname.find());
+              //   router.push(`${router.asPath}?page=${page}`);
+            }}
+          />
+        </Stack>
+      )}
+    </>
+  );
+}
