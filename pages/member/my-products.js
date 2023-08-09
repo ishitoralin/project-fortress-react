98684b4b3ee58437e10e58e9f1b4b2ec0c0d2272
LQTjim <jim6050482@hotmail.com>
2023-08-09 16:21:18 +0800

    UPDATE:my-product

--- pages/member/my-products.js
+++ pages/member/my-products.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import MemberCenterLayout from '@/components/layout/memberCenterLayout';
 import MyProductsComponent from '@/components/member/my-products';
 
